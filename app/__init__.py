072d85f5d40325821dfffdd112ed560d5613814f
janicelichtman <jalichtman@gmail.com>
2021-06-06 17:40:21 -0700

    Added GET, POST to books, and GET, PUT, PATCH, DELETE to books/<book_id>

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,24 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app(test_config=None):
     app = Flask(__name__)
 
+    # registering blueprints which create  routes
+    from .routes import books_bp
+    app.register_blueprint(books_bp)
+
+    # setting up the connection to the database
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    
+    from app.models.book import Book
+        
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,64 @@
-from flask import Blueprint
+from app import db
+from app.models.book import Book
+from flask import Blueprint, request, make_response, jsonify
 
+
+books_bp = Blueprint("books", __name__, url_prefix="/books")
+
+@books_bp.route("", strict_slashes=False, methods=["GET", "POST"])
+def handle_books():
+    if request.method == "GET":
+        books = Book.query.all()
+        books_response = []
+        for book in books:
+            books_response.append({
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            })
+        return jsonify(books_response)
+    elif request.method == "POST":
+        request_body = request.get_json()
+        new_book = Book(title=request_body["title"],
+                        description=request_body["description"])
+
+        db.session.add(new_book)
+        db.session.commit()
+
+        return make_response(f"Book {new_book.title} successfully created", 201)
+
+
+@books_bp.route("/<book_id>", strict_slashes=False, methods=["GET", "PUT", "PATCH", "DELETE"])
+def handle_book(book_id):
+    book = Book.query.get(book_id)
+    if not book:
+        return make_response(f"Book {book_id} not found", 404)
+
+    if request.method == "GET":
+        return {
+            "id": book.id,
+            "title": book.title,
+            "description": book.description
+        }
+    elif request.method == "PUT":
+        form_data = request.get_json()
+        book.title = form_data["title"],
+        book.description = form_data["description"]
+
+        db.session.commit()
+
+        return make_response(f"Book {book_id} successfully updated")
+    
+    elif request.method == "PATCH":
+        form_data = request.get_json()
+        if "title" in form_data.keys():
+            book.title = form_data["title"]
+        if "description" in form_data.keys():
+            book.title = form_data["title"]
+        db.session.commit()       
+        return make_response(f"Book {book_id} successfully updated")
+
+    elif request.method == "DELETE":
+        db.session.delete(book)
+        db.session.commit()
+        return make_response(f"Book {book_id} successfully deleted")
