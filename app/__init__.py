a9dabe7e939b505825135585f747ba2a74e9f771
janicelichtman <jalichtman@gmail.com>
2021-06-07 23:32:18 -0700

    set up pytest and added first test to /books

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,33 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
 
 db = SQLAlchemy()
 migrate = Migrate()
+load_dotenv()
 
 def create_app(test_config=None):
     app = Flask(__name__)
 
-    # registering blueprints which create  routes
-    from .routes import books_bp
-    app.register_blueprint(books_bp)
-
-    # setting up the connection to the database
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
-
+    if not test_config:
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+            "SQLALCHEMY_DATABASE_URI")
+    else:
+        app.config["TESTING"] = True
+        app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+            "SQLALCHEMY_TEST_DATABASE_URI")
+        
     db.init_app(app)
     migrate.init_app(app, db)
     
     from app.models.book import Book
+    
+    # registering blueprints which create  routes
+    from .routes import books_bp
+    app.register_blueprint(books_bp)
         
     return app
--- app/routes.py
+++ app/routes.py
@@ -8,7 +8,12 @@ books_bp = Blueprint("books", __name__, url_prefix="/books")
 @books_bp.route("", strict_slashes=False, methods=["GET", "POST"])
 def handle_books():
     if request.method == "GET":
-        books = Book.query.all()
+        title_query = request.args.get("title")
+        if title_query:
+            books = Book.query.filter_by(title=title_query)
+        else: 
+            books = Book.query.order_by(Book.id).all()
+        # books = Book.query.filter(Book.id >= 20).all() # Example of filtering
         books_response = []
         for book in books:
             books_response.append({
@@ -24,8 +29,7 @@ def handle_books():
 
         db.session.add(new_book)
         db.session.commit()
-
-        return make_response(f"Book {new_book.title} successfully created", 201)
+        return make_response(f"Book {new_book.title} successfully created with id {new_book.id}", 201)
 
 
 @books_bp.route("/<book_id>", strict_slashes=False, methods=["GET", "PUT", "PATCH", "DELETE"])
@@ -51,10 +55,15 @@ def handle_book(book_id):
     
     elif request.method == "PATCH":
         form_data = request.get_json()
-        if "title" in form_data.keys():
-            book.title = form_data["title"]
-        if "description" in form_data.keys():
-            book.title = form_data["title"]
+        
+        for key, value in form_data.items():
+            if key in dir(book):  # nod towards data validation - checking that given key is a valid attribute 
+                setattr(book, key, value)
+        # Alternative, more explicit method:
+        # if "title" in form_data.keys():
+        #     book.title = form_data["title"]
+        # if "description" in form_data.keys():
+        #     book.description = form_data["description"]
         db.session.commit()       
         return make_response(f"Book {book_id} successfully updated")
 
@@ -62,3 +71,16 @@ def handle_book(book_id):
         db.session.delete(book)
         db.session.commit()
         return make_response(f"Book {book_id} successfully deleted")
+    
+@books_bp.route("/top_three", strict_slashes=False, methods=["GET"])
+def handle_top_three_books():
+        books = Book.query.order_by(Book.id).limit(3).all()
+        books_response = []
+        for book in books:
+            books_response.append({
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            })
+        return jsonify(books_response)
+    
