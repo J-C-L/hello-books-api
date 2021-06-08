e433ded551e6a220c1574e85092ede8858c9eda9
janicelichtman <jalichtman@gmail.com>
2021-06-08 00:02:31 -0700

    added more tests for /books and /books/<book_id>

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -7,6 +7,42 @@ def test_get_all_books_with_no_records(client):
     assert response.status_code == 200
     assert response_body == []
     
+def test_get_all_books_with_records(client, two_saved_books):
+    # Act
+    response = client.get("/books")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == [
+        {
+        "id": 1,
+        "title": "Ocean Book",
+        "description": "watr 4evr"
+        },
+        {
+        "id": 2,
+        "title": "Mountain Book",
+        "description": "i luv 2 climb rocks"
+        } 
+    ]   
+
+def test_create_new_book(client, ):
+    # Act
+    response = client.post("/books", 
+        json={
+        "title" : "Happy Book",
+        "description" : "shiny happy people"
+        }
+        )
+    response_text = response.get_data(as_text=True)
+
+    # Assert
+    assert response.status_code == 201
+    assert response_text == "Book Happy Book successfully created with id 1"
+
+    
+    
 def test_get_one_book(client, two_saved_books):
     # Act
     response = client.get("/books/1")
@@ -19,3 +55,11 @@ def test_get_one_book(client, two_saved_books):
         "title": "Ocean Book",
         "description": "watr 4evr"
     }
+    
+def test_get_one_book_no_data(client):
+    # Act
+    response = client.get("/books/1")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 404
